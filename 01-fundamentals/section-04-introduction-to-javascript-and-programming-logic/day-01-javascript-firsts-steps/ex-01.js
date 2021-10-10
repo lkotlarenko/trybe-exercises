e103e3f6d519b87ab6461b2bd731998eca9bdbdd
lkotlarenko <lkotlarenko@protonmail.com>
2021-10-10 17:11:28 -0300

    Exercicio 3 + Fixing some formating

--- 01-fundamentals/section-04-introduction-to-javascript-and-programming-logic/day-01-javascript-firsts-steps/ex-01.js
+++ 01-fundamentals/section-04-introduction-to-javascript-and-programming-logic/day-01-javascript-firsts-steps/ex-01.js
@@ -1,17 +1,17 @@
-let a = 5
-let b = 10
+let a = 5;
+let b = 10;
 
 // Adição
-console.log(`${a} mais ${b} = ${a + b}`)
+console.log(`${a} mais ${b} = ${a + b}`);
 
-// Subtração 
-console.log(`${a} menos ${b} = ${a - b}`)
+// Subtração
+console.log(`${a} menos ${b} = ${a - b}`);
 
 // Multiplicação
-console.log(`${a} vezes ${b} = ${a * b}`)
+console.log(`${a} vezes ${b} = ${a * b}`);
 
-// Divisão 
-console.log(`${a} dividido por ${b} = ${a / b}`)
+// Divisão
+console.log(`${a} dividido por ${b} = ${a / b}`);
 
-// Módulo 
-console.log(`${a} dividido por ${b} sobra ${a % b}`)
+// Módulo
+console.log(`${a} dividido por ${b} sobra ${a % b}`);
