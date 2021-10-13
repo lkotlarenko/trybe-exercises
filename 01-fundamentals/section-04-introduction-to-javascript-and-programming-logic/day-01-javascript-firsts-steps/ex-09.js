d6e90417b75ea78630dbcc4d76b2dd9f83dd3d04
lkotlarenko <lkotlarenko@protonmail.com>
2021-10-13 12:51:25 -0300

    Exercicio 9

---
+++ 01-fundamentals/section-04-introduction-to-javascript-and-programming-logic/day-01-javascript-firsts-steps/ex-09.js
@@ -0,0 +1,9 @@
+const a = 2;
+const b = 4;
+const c = 5;
+
+let ehImpar = false;
+a % 2 !== 0 ? ehImpar = true : null;
+b % 2 !== 0 ? ehImpar = true : null;
+c % 2 !== 0 ? ehImpar = true : null;
+console.log(ehImpar)
