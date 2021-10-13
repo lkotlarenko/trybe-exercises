dfda0e296e320f44c65314f23ec5832121ad84b2
lkotlarenko <lkotlarenko@protonmail.com>
2021-10-13 19:46:00 -0300

    Exercicio 7

--- 01-fundamentals/section-04-introduction-to-javascript-and-programming-logic/day-02-javascript-array-and-loop-for/ex-07.js
+++ 01-fundamentals/section-04-introduction-to-javascript-and-programming-logic/day-02-javascript-array-and-loop-for/ex-07.js
@@ -1,1 +1,7 @@
+// Utilizando for, descubra qual o menor valor contido no array e imprima-o;
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let menorNumero = numbers[0];
+for (let i = 0; i != numbers.length; i += 1) {
+  numbers[i] <= menorNumero ? (menorNumero = numbers[i]) : null;
+}
+console.log(menorNumero);
