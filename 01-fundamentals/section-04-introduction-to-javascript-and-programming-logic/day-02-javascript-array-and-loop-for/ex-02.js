92362af2022e612fe13e023d1e81a17d8ce7b513
lkotlarenko <lkotlarenko@protonmail.com>
2021-10-13 19:11:53 -0300

    Exercicio 2

--- 01-fundamentals/section-04-introduction-to-javascript-and-programming-logic/day-02-javascript-array-and-loop-for/ex-02.js
+++ 01-fundamentals/section-04-introduction-to-javascript-and-programming-logic/day-02-javascript-array-and-loop-for/ex-02.js
@@ -1,1 +1,11 @@
+// Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let total = 0;
+total = numbers.reduce((acc, cur) => acc + cur);
+
+// ou:
+// for (let i = 0; i != numbers.length; i += 1) {
+//   total = total + numbers[i];
+// }
+
+console.log("Total: ", total);
