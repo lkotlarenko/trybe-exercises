296588af19e1a3889d0c40e4d2edf5108dabc0d2
lkotlarenko <lkotlarenko@protonmail.com>
2021-10-13 12:47:41 -0300

    Exercicio 8

---
+++ 01-fundamentals/section-04-introduction-to-javascript-and-programming-logic/day-01-javascript-firsts-steps/ex-08.js
@@ -0,0 +1,9 @@
+const a = 3;
+const b = 5;
+const c = 7;
+
+let ehPar = false;
+a % 2 === 0 ? ehPar = true : null;
+b % 2 === 0 ? ehPar = true : null;
+c % 2 === 0 ? ehPar = true : null;
+console.log(ehPar)
