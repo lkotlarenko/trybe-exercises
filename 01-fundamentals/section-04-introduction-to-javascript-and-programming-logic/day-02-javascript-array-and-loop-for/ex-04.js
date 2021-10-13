b30d5a635bd3a50fb6b6a07b4120b6ea2d49d653
lkotlarenko <lkotlarenko@protonmail.com>
2021-10-13 19:27:31 -0300

    Exercicio 4

--- 01-fundamentals/section-04-introduction-to-javascript-and-programming-logic/day-02-javascript-array-and-loop-for/ex-04.js
+++ 01-fundamentals/section-04-introduction-to-javascript-and-programming-logic/day-02-javascript-array-and-loop-for/ex-04.js
@@ -1,1 +1,6 @@
+// Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+const total = numbers.reduce((acc, cur) => acc + cur);
+let mediaAritmetica = total / numbers.length;
+console.log(mediaAritmetica);
+mediaAritmetica < 20 ? console.log("valor menor que 20") : console.log("valor maior que 20");
