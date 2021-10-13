d53e9bd6687715338ba8c49ddbb79e7570246c72
lkotlarenko <lkotlarenko@protonmail.com>
2021-10-13 19:42:23 -0300

    Exercicio 6

--- 01-fundamentals/section-04-introduction-to-javascript-and-programming-logic/day-02-javascript-array-and-loop-for/ex-06.js
+++ 01-fundamentals/section-04-introduction-to-javascript-and-programming-logic/day-02-javascript-array-and-loop-for/ex-06.js
@@ -1,1 +1,9 @@
+// Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numImpares = 0;
+for (let i = 0; i != numbers.length; i += 1) {
+  numbers[i] % 2 !== 0 ? (numImpares += 1) : null;
+}
+numImpares === 0
+  ? console.log("nenhum valor ímpar encontrado")
+  : console.log(numImpares);
