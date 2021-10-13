ee22388ee66e8d8137c3a3d42e2c025a33d3551c
lkotlarenko <lkotlarenko@protonmail.com>
2021-10-13 12:33:02 -0300

    Exercicio 7

---
+++ 01-fundamentals/section-04-introduction-to-javascript-and-programming-logic/day-01-javascript-firsts-steps/ex-07.js
@@ -0,0 +1,17 @@
+const nota = -1;
+
+if (nota < 0 || nota > 100) {
+  console.log("Erro nota invalida, valores validos: (0-100)");
+} else if (nota >= 90) {
+  console.log("A");
+} else if (nota >= 80) {
+  console.log("B");
+} else if (nota >= 70) {
+  console.log("C");
+} else if (nota >= 60) {
+  console.log("D");
+} else if (nota >= 50) {
+  console.log("E");
+} else {
+  console.log("F");
+}
