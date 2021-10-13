36a699aa4517fab946f77356c07f7a8cbf3d0557
lkotlarenko <lkotlarenko@protonmail.com>
2021-10-13 18:51:52 -0300

    Exercicio 1 + criação de todos os arquivos a serem usados

---
+++ 01-fundamentals/section-04-introduction-to-javascript-and-programming-logic/day-02-javascript-array-and-loop-for/ex-01.js
@@ -0,0 +1,6 @@
+// Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let i = 0; i != numbers.length; i += 1) {
+  console.log(numbers[i]);
+}
