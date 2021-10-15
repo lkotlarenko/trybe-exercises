50251eb371103f543cec320a909880f4cb734c2b
lkotlarenko <lkotlarenko@protonmail.com>
2021-10-15 18:32:16 -0300

    Exercicio 4

---
+++ 01-fundamentals/section-04-introduction-to-javascript-and-programming-logic/day-04-objects-and-functions/ex-04.js
@@ -0,0 +1,16 @@
+// Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
+// Margarida
+// Pato Donald
+// Namorada do personagem principal nos quadrinhos do Pato Donald
+// Sim
+
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  recorrente: "Sim",
+};
+
+for (i in info) {
+  console.log(info[i]);
+}
