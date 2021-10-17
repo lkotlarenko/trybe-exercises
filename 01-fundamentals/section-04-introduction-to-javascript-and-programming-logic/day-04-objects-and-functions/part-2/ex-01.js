c597fe0ec4a1c612b2ab8334efab2079a6327587
lkotlarenko <lkotlarenko@protonmail.com>
2021-10-17 00:26:00 -0300

    Exercicios pt2: 2 + fix ex1

--- 01-fundamentals/section-04-introduction-to-javascript-and-programming-logic/day-04-objects-and-functions/part-2/ex-01.js
+++ 01-fundamentals/section-04-introduction-to-javascript-and-programming-logic/day-04-objects-and-functions/part-2/ex-01.js
@@ -1,11 +1,13 @@
+// Crie uma função que receba uma string e retorne true se for um palíndromo, ou false, se não for.
+
 function verificaPalindrome(palavra) {
-  let palindrome = false;
+  let palindrome = true;
   let i = -1;
   for (let i2 = palavra.length - 1; i2 != 0; i2 -= 1) {
     i += 1;
-    palavra[i] === palavra[i2] ? (palindrome = true) : (palindrome = false);
-    console.log(`${palavra[i]} e ${palavra[i2]}: ${palindrome}`);
+    palavra[i] === palavra[i2] ? null : (palindrome = false);
+    console.log(`${i}-${palavra[i]} vs ${i2}-${palavra[i2]} -> ${palindrome}`);
   }
   return palindrome;
 }
-verificaPalindrome("arara");
+console.log(`é palindrome? -> ${verificaPalindrome("alabula")}`);
