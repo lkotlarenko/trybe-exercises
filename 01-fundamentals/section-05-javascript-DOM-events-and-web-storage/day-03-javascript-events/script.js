d248336e27c5bce36d415927a2ddeba78e932229
lkotlarenko <lkotlarenko@protonmail.com>
2021-10-21 20:24:33 -0300

    Exercise 7

--- 01-fundamentals/section-05-javascript-DOM-events-and-web-storage/day-03-javascript-events/script.js
+++ 01-fundamentals/section-05-javascript-DOM-events-and-web-storage/day-03-javascript-events/script.js
@@ -129,3 +129,14 @@ function dayMouseOut() {
   });
 }
 dayMouseOut();
+
+// 7. Implemente uma função que adiciona uma tarefa personalizada ao calendário. A função deve receber como parâmetro a string com o nome da tarefa (ex: "cozinhar") e criar dinamicamente um elemento com a tag <span> contendo a tarefa.
+// O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks".
+
+function createTask(task) {
+  let taskParent = document.querySelector(".my-tasks");
+  let newTask = document.createElement("span");
+  newTask.innerHTML = task;
+  taskParent.appendChild(newTask);
+}
+createTask("Entrar no course da Trybe");
