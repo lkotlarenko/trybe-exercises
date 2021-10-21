73b23e172d8d7d444f0cc6136f481bfd1096d020
lkotlarenko <lkotlarenko@protonmail.com>
2021-10-21 18:38:43 -0300

    Creating files

---
+++ 01-fundamentals/section-05-javascript-DOM-events-and-web-storage/day-03-javascript-events/script.js
@@ -0,0 +1,16 @@
+function createDaysOfTheWeek() {
+    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    const weekDaysList = document.querySelector('.week-days');
+  
+    for (let index = 0; index < weekDays.length; index += 1) {
+      const days = weekDays[index];
+      const dayListItem = document.createElement('li');
+      dayListItem.innerHTML = days;
+  
+      weekDaysList.appendChild(dayListItem);
+    };
+  };
+  
+  createDaysOfTheWeek();
+  
+  // Escreva seu código abaixo.
